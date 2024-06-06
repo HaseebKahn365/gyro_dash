e0162fd463b2cf49d0f9f589b69c13891cc59336
Abdul Haseeb <58126830+HaseebKahn365@users.noreply.github.com>
2024-06-06 20:44:45 +0500

    updated the sensor

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 // ignore_for_file: deprecated_member_use
 
 import 'dart:async';
-import 'dart:developer';
+import 'dart:developer' as developer;
+import 'dart:math';
 
 import 'package:flame/components.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_compass/flutter_compass.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
 //we are gonna have a dingle screen in this game:
@@ -97,7 +99,14 @@ class GameSimulator extends FlameGame {
     );
   }
 
+  //adding a stream for compass events
+  late StreamSubscription<CompassEvent> _compassStream;
+
   final Player myAppPlayer = Player();
+
+  final dot = Dot();
+
+  var angle = 0.0;
   @override
   void onMount() {
     super.onMount();
@@ -112,26 +121,40 @@ class GameSimulator extends FlameGame {
       paint: Paint()..color = Color.fromARGB(255, 255, 253, 160).withOpacity(0.5),
     ));
 
+    world.add(dot);
+
+    //adding the compass stream
+    _compassStream = FlutterCompass.events!.listen((CompassEvent event) {
+      myAppPlayer.text2.text = 'Magnetometer Stream: ${event.heading}';
+      myAppPlayer.text2.position = Vector2(80, 80);
+      angle = event.heading!;
+    });
+
     /*
     Now we need to fill the world with lots of dots to show the ground. We will use the magnetometer sensor to rotate the world.
     lets create lots of dots in the world.
     
      */
 
-    world.addAll(dotGrid());
-
     camera.follow(myAppPlayer);
   }
 
   @override
   void update(double dt) {
-    super.update(dt);
-  }
+    // developer.log('angle: $angle');
 
+    dot.angle = angle * (3.14159 / 180); // Convert angle to radians and apply a factor to slow down the rotation
+
+    super.update(dt);
+    //rotate the entire grid using the angle
+  }
+}
+
+class Dot extends PositionComponent {
   List<RectangleComponent> dotGrid() {
     List<RectangleComponent> dots = [];
-    for (int i = -600; i < 300; i += 50) {
-      for (int j = -1000; j < 500; j += 50) {
+    for (int i = -6000; i < 6000; i += 150) {
+      for (int j = -10000; j < 7000; j += 150) {
         dots.add(
           RectangleComponent(
             size: Vector2(5, 5),
@@ -145,11 +168,16 @@ class GameSimulator extends FlameGame {
     }
     return dots;
   }
+
+  @override
+  void onLoad() {
+    addAll(dotGrid());
+    super.onMount();
+  }
 }
 
 class Player extends PositionComponent with HasGameRef<GameSimulator> {
   late StreamSubscription<GyroscopeEvent> _gyroscopeStream; //gyroscope stream
-  late StreamSubscription<MagnetometerEvent> _magnetometerStream; //magnetometer stream
   var _velocity = Vector2(0, 0); //velocity of the player
   late TextComponent text; // Declare text component here
   late TextComponent text2; // Declaring my text component here for the magnetometer
@@ -165,7 +193,7 @@ class Player extends PositionComponent with HasGameRef<GameSimulator> {
     );
 
     text2 = TextComponent(
-      text: 'velocity due to magnetometer is: 0, 0',
+      text: 'magnetometer info is: 0, 0',
       scale: Vector2(0.7, 0.7),
     );
     //static postion for the text component
@@ -173,13 +201,8 @@ class Player extends PositionComponent with HasGameRef<GameSimulator> {
       _velocity = Vector2(event.y, event.x);
       text.text = 'Gyro Stream: ${event.x.toStringAsFixed(3)}, ${event.y.toStringAsFixed(3)}';
       text.position = Vector2(80, 60);
+      position += _velocity;
     });
-
-    //magnetometer stream
-    // _magnetometerStream = magnetometerEvents.listen((MagnetometerEvent event) {
-    //   text2.text = 'Magnetometer Stream: ${event.x.toStringAsFixed(3)}, ${event.y.toStringAsFixed(3)}';
-    //   text2.position = Vector2(60, 80);
-    // });
 
     gameRef.add(text);
     gameRef.add(text2);
@@ -191,8 +214,13 @@ class Player extends PositionComponent with HasGameRef<GameSimulator> {
   void update(double dt) {
     super.update(dt);
     //updating my position
-    position += _velocity * dt * 100;
+    //we need to add translational motion to the player based on the gyroscope sensor
     // log('Player position: $position');
+
+    //calculating the offset of the player from the center and adding this offset the position of the player
+    //this will make the player move in the direction of the gyroscope sensor
+
+    // developer.log('Player position: $position');
 
     //show in app the player position using the text just above the player
     // log('Player position: $position');
@@ -207,9 +235,64 @@ class Player extends PositionComponent with HasGameRef<GameSimulator> {
   }
 }
 
-/*
 
 
-Now we are gonna keep the player at the center while allowing for the rotation of the world using the magnetometer sensor.
 
+/*import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart';
+
+class CompassEvent {
+  // The heading, in degrees, of the device around its Z
+  // axis, or where the top of the device is pointing.
+  final double? heading;
+
+  // The heading, in degrees, of the device around its X axis, or
+  // where the back of the device is pointing.
+  final double? headingForCameraMode;
+
+  // The deviation error, in degrees, plus or minus from the heading.
+  // NOTE: for iOS this is computed by the platform and is reliable. For
+  // Android several values are hard-coded, and the true error could be more
+  // or less than the value here.
+  final double? accuracy;
+
+  CompassEvent.fromList(List<double>? data)
+      : heading = data?[0] ?? null,
+        headingForCameraMode = data?[1] ?? null,
+        accuracy = (data == null) || (data[2] == -1) ? null : data[2];
+
+  @override
+  String toString() {
+    return 'heading: $heading\nheadingForCameraMode: $headingForCameraMode\naccuracy: $accuracy';
+  }
+}
+
+/// [FlutterCompass] is a singleton class that provides assess to compass events
+/// The heading varies from 0-360, 0 being north.
+class FlutterCompass {
+  static final FlutterCompass _instance = FlutterCompass._();
+
+  factory FlutterCompass() {
+    return _instance;
+  }
+
+  FlutterCompass._();
+
+  static const EventChannel _compassChannel =
+      const EventChannel('hemanthraj/flutter_compass');
+  static Stream<CompassEvent>? _stream;
+
+  /// Provides a [Stream] of compass events that can be listened to.
+  static Stream<CompassEvent>? get events {
+    if (kIsWeb) {
+      return Stream.empty();
+    }
+    _stream ??= _compassChannel
+        .receiveBroadcastStream()
+        .map((dynamic data) => CompassEvent.fromList(data?.cast<double>()));
+    return _stream;
+  }
+}
  */
